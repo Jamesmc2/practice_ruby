28b8ec83aff039157bfad6da946e0d3ad1b816d9
jimmy <jamesmc21998@gmail.com>
2023-07-25 13:58:17 -0500

    add 3 store items in hashes

---
+++ store_item.rb
@@ -0,0 +1,27 @@
+item1 = {
+  name: "basketball",
+  color: "orange",
+  price: 10,
+  quantity: 50,
+}
+p item1
+
+item2 = {
+  name: "cleats",
+  color: "black",
+  price: 50,
+  quantity: 10,
+}
+
+p item2
+
+item3 = {
+  name: "lawn mower",
+  color: "red",
+  price: 200,
+  quantity: 5,
+}
+
+p item3
+
+p "The #{item3[:name]} is #{item3[:color]} and costs $#{item3[:price]}. We have #{item3[:quantity]} in stock."
